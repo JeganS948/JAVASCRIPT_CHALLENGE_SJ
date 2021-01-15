46da4ce24edbbaf6292decd425946daf3e9ebb39
JeganS948 <sharanvika.jegatheeswaran@mail.utoronto.ca>
2021-01-15 00:40:44 -0500

    Update app.js

--- UFO-LEVEL-1/static/js/app.js
+++ UFO-LEVEL-1/static/js/app.js
@@ -1,9 +1,25 @@
 // from data.js
 var tableData = data;
-let tableMatch = null;  
+let tableMatch = null;
+console.log(tableData);
 
-// Variable Declariation
+// Variable declariation
 let tbody = d3.select("tbody");
 let filtered = 0;
 
-// YOUR CODE HERE!
+// Build table
+function buildTable(data){
+    // Clear existing data
+    tbody.html("");
+    // Loop through `data` 
+    data.forEach((dataRow) => {
+        // Append table row `tr` for each UFO sighting value
+        let row = tbody.append("tr");
+        // Use `Object.values` & `forEach` to 
+        Object.values(dataRow).forEach((val) => {
+            // Append a cell to the row for each value
+            let cell = row.append("td");
+            cell.text(val);
+        });
+    })
+}
